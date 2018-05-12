97cdb064576e2be560481a9e11f0ad550d5ffde2
chris48s <chris.shaw480@gmail.com>
2018-05-12 20:38:45 +0100

    use markdown for long description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 def _get_description():
     try:
-        path = os.path.join(os.path.dirname(__file__), 'README.rst')
+        path = os.path.join(os.path.dirname(__file__), 'README.md')
         with open(path, encoding='utf-8') as f:
             return f.read()
     except IOError:
@@ -21,6 +21,7 @@ setup(
     packages=['eco_parser'],
     description='Parse ward lists from Electoral Change Orders on http://www.legislation.gov.uk/',
     long_description=_get_description(),
+    long_description_content_type="text/markdown",
     entry_points={
         'console_scripts': [
             'eco_parser = eco_parser.__main__:main'
