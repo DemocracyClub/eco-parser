d66308ef052025d629c5a02b8dab3f3cacda4a92
chris48s <chris.shaw480@gmail.com>
2019-10-16 18:55:47 +0100

    run the tests on python 3.8, drop python 3.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,14 @@
 # Changelog
 
+## :package: [0.2.0](https://pypi.org/project/eco-parser/0.2.0/) - 2019-10-16
+
+* Tested on Python 3.8
+* Drop testing on Python 3.4
+
 ## :package: [0.1.1](https://pypi.org/project/eco-parser/0.1.1/) - 2018-12-09
 
-Tested on Python 3.7
+* Tested on Python 3.7
 
 ## :package: [0.1.0](https://pypi.org/project/eco-parser/0.1.0/) - 2018-01-29
 
-First Release
+* First Release
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def _get_description():
 
 setup(
     name="eco_parser",
-    version="0.1.1",
+    version="0.2.0",
     author="chris48s",
     license="MIT",
     url="https://github.com/DemocracyClub/eco-parser/",
@@ -31,9 +31,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
